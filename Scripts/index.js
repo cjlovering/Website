6c0a294cecb1fb5e7a3a3ab21c955f715e5efc31
cjlovering <cjlovering@130.215.14.76>
2015-12-30 00:08:29 -0500

    Added functionality for switching imbetween tabs. Added classes and design. (Mapped them).

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,5 +1,9 @@
 //index script
 $(document).ready(function(){
+  
+
+  var pages = [];
+
   $.get( 'scripts/template.html' ).then( function ( template ) {
 
     var ractive = new Ractive({
@@ -8,8 +12,10 @@ $(document).ready(function(){
       template: template,
 
       data: { 
-        name: 'Charlie Lovering',
-        sections: []
+        name : 'Charlie Lovering',
+        sections : [],
+        current_page : 0,
+        classes : []
       }
     });
 
@@ -17,5 +23,25 @@ $(document).ready(function(){
     $.when(resources).done(function(r){
       ractive.set('sections', r.sections);
     });
+
+    var classes_load = $.getJSON('messages/classes.json');
+    $.when(classes_load).done(function(r){
+      console.log(r);
+      console.log(r.classes);
+      ractive.set('classes', r.classes);
+    })
+
+    ractive.on( 'toggle', function ( event ) {
+      this.toggle( event.keypath + '.expanded' );
+    });
+
+    ractive.on({
+      show: function ( event, which ) {
+        if(which != ractive.get('current_page'))
+        ractive.set( 'current_page', null ).then( function () {
+          ractive.set( 'current_page', which );
+          });
+
+      }});
   });
 });
