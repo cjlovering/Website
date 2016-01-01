d70a935cc8b06e6797c898c927ac472bf0532225
cjlovering <cjlovering@130.215.14.76>
2016-01-01 01:16:25 -0500

    further fixing capitilzation

--- Scripts/index.js
+++ Scripts/index.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
   var pages = [];
 
-  $.get( 'scripts/template.html' ).then( function ( template ) {
+  $.get( 'Scripts/template.html' ).then( function ( template ) {
 
     var ractive = new Ractive({
       el: '#containerTotal',
@@ -26,12 +26,12 @@ $(document).ready(function(){
       }
     });
 
-    var resources = $.getJSON('messages/resources.json');
+    var resources = $.getJSON('Messages/resources.json');
     $.when(resources).done(function(r){
       ractive.set('sections', r.sections);
     });
 
-    var info = $.getJSON('messages/info.json');
+    var info = $.getJSON('Messages/info.json');
     $.when(info).done(function(r){
       ractive.set('jobs', r.jobs);
       ractive.set('classes', r.classes);
